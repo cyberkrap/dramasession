dbf9ddb21fb352d419dce1d31d216eb0db6d4500
jewspy <139158862+jewspy@users.noreply.github.com>
2026-07-22 20:58:07 +0300

    Hide removed artwork from galleries and site banners

--- files/routes/community_gallery.py
+++ files/routes/community_gallery.py
@@ -4,11 +4,15 @@ from files.__main__ import app, limiter
 from files.helpers.cloudflare import purge_files_in_cache
 from files.helpers.community_assets import (
 	COMMUNITY_ASSET_CONFIG,
+	active_community_asset_filenames,
 	list_approved_assets,
 	remove_approved_asset,
 )
 from files.helpers.config.const import DEFAULT_RATELIMIT, DEFAULT_RATELIMIT_SLOWER, PERMS, SITE_FULL
 from files.routes.wrappers import admin_level_required, auth_desired
+
+
+app.jinja_env.globals["active_community_asset_filenames"] = active_community_asset_filenames
 
 
 @app.get("/terms")
