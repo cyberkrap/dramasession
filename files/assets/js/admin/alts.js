57d49a69b409e42a67086974b7f405800ccf0f82
jewspy <139158862+jewspy@users.noreply.github.com>
2026-07-25 14:03:58 +0300

    Reload alt actions from saved server state

--- files/assets/js/admin/alts.js
+++ files/assets/js/admin/alts.js
@@ -1,3 +1,34 @@
+function parseAltResponse(xhr) {
+	try {
+		return JSON.parse(xhr.response);
+	}
+	catch (e) {
+		console.log(e);
+		return null;
+	}
+}
+
+function setAltDeletedState(element, username, otherId, shouldDelink) {
+	element.disabled = true;
+	element.classList.add('disabled');
+
+	const method = shouldDelink ? 'PUT' : 'DELETE';
+	const xhr = createXhrWithFormKey(`/@${username}/alts/${otherId}/deleted`, method, new FormData());
+	xhr[0].onload = function() {
+		const data = parseAltResponse(xhr[0]);
+		if (xhr[0].status >= 200 && xhr[0].status < 300) {
+			showToast(true, getMessageFromJsonData(true, data));
+			location.reload();
+			return;
+		}
+
+		showToast(false, getMessageFromJsonData(false, data));
+		element.disabled = false;
+		element.classList.remove('disabled');
+	};
+	xhr[0].send(xhr[1]);
+}
+
 function submitAddAlt(element, username) {
 	const isLinking = element.id == 'add-alt-form-link';
 	const otherElement = isLinking ? document.getElementById('add-alt-form-delink') : document.getElementById('add-alt-form-link');
@@ -11,13 +42,7 @@ function submitAddAlt(element, username) {
 	form.append('other_username', document.getElementById('link-input-other').value);
 	const xhr = createXhrWithFormKey(`/@${username}/alts/`, 'POST', form);
 	xhr[0].onload = function() {
-		let data;
-		try {
-			data = JSON.parse(xhr[0].response);
-		}
-		catch(e) {
-			console.log(e);
-		}
+		const data = parseAltResponse(xhr[0]);
 		if (xhr[0].status >= 200 && xhr[0].status < 300) {
 			showToast(true, getMessageFromJsonData(true, data));
 			location.reload();
@@ -28,6 +53,6 @@ function submitAddAlt(element, username) {
 			element.classList.remove('disabled');
 			otherElement.classList.remove('disabled');
 		}
-	}
+	};
 	xhr[0].send(xhr[1]);
 }
